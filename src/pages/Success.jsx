11b8d5a3d17c68584a70d8f3452fb11eb2cb4821
Project-STS <wahyuganss33@gmail.com>
2025-09-02 22:34:56 +0700

    Rename page success dan install skeleton react

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/success.css";
+import "../styles/Success.css";
 
 function Success() {
   const navigate = useNavigate();
