0b6a93687ad788e770af36f312df6d19d5bb85d0
Project-STS <wahyuganss33@gmail.com>
2025-09-05 11:39:08 +0700

    Update connect to api

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,16 +5,20 @@ import "../styles/product.css";
 function ProductCard({ product }) {
   return (
     <Link to={`/product/${product.id}`} className="product-card">
-      <img src={product.image} alt={product.name} className="product-img" />
+      <img
+        src={product.image_url || "/img/no-image.png"}
+        alt={product.name}
+        className="product-img"
+      />
       <div className="product-body">
         <h3 className="product-title">{product.name}</h3>
-        <p className="price">Rp{product.price.toLocaleString()}</p>
+        <p className="price">Rp{product.price.toLocaleString("id-ID")}</p>
         <div className="product-info">
           <p>
-            <FaStar color="gold" /> {product.rating} | {product.sold}
+            <FaStar color="gold" /> {product.rating || 0} | {product.sold || 0}
           </p>
           <p>
-            <FaMapMarkerAlt /> {product.location}
+            <FaMapMarkerAlt /> {product.location || "Jakarta"}
           </p>
         </div>
       </div>
