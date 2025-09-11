47df08e782036586dbae7a02587a1f49df19af3c
Project-STS <wahyuganss33@gmail.com>
2025-09-11 15:44:17 +0700

    Update Edit skeleton

--- src/components/ProductSkeleton.jsx
+++ src/components/ProductSkeleton.jsx
@@ -4,8 +4,11 @@ function ProductSkeleton() {
   return (
     <div className="skeleton-card">
       <div className="skeleton-img"></div>
-      <div className="skeleton-text short"></div>
-      <div className="skeleton-text long"></div>
+      <div className="skeleton-body">
+        <div className="skeleton-text short"></div>
+        <div className="skeleton-text medium"></div>
+        <div className="skeleton-text long"></div>
+      </div>
     </div>
   );
 }
