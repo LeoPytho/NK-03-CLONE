f9fb2294436ececff654015b678acf0b0b312f01
LeoPytho <108222261+LeoPytho@users.noreply.github.com>
2025-09-26 15:23:42 +0700

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -195,10 +195,10 @@ const ProfilePage = () => {
       
       // Try to get profile using stored username first, then email
       if (storedData.username) {
-        url = `https://v2.jkt48connect.com/api/profiles/public/profile/username/${encodeURIComponent(storedData.username)}?username=vzy&password=vzy`;
+        url = `https://v2.jkt48connect.com/api/dashboard/public/profile/username/${encodeURIComponent(storedData.username)}?username=vzy&password=vzy`;
         searchValue = storedData.username;
       } else if (storedData.email) {
-        url = `https://v2.jkt48connect.com/api/profiles/public/profile/email/${encodeURIComponent(storedData.email)}?username=vzy&password=vzy`;
+        url = `https://v2.jkt48connect.com/api/dashboard/public/profile/email/${encodeURIComponent(storedData.email)}?username=vzy&password=vzy`;
         searchValue = storedData.email;
       } else {
         throw new Error('No username or email found in stored data');
@@ -338,7 +338,7 @@ const ProfilePage = () => {
         const storedData = getStoredData();
         let identifier = storedData.username || storedData.email;
 
-        const response = await fetch('https://v2.jkt48connect.com/api/profiles/update-address/search', {
+        const response = await fetch('https://v2.jkt48connect.com/api/dashboard/update-address/search', {
           method: 'PUT',
           headers: {
             'Content-Type': 'application/json',
